197ce9cbe9eea762cc8d88d157bc40a67960d6e7
Batanda Kayondo <bkayondo@fltug.com>
2019-07-10 17:48:51 +0300

    implemented knn algorithm to predict ball dropped at 350

--- score.js
+++ score.js
@@ -1,13 +1,31 @@
 const outputs = [];
+const predictionPoint = 350;
+const k =6;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
 
-  console.log(outputs);
 }
 
 function runAnalysis() {
   // Write code here to analyze stuff
+  const bucket = _.chain(outputs)
+	  .map(row => [dist4mPoint(row[0]), row[3]]) //absolute difference and bucket ball fell into
+	  .sortBy(row => row[0]) // sort by abs difference, from least to greatest
+	  .slice(0, k) // take k number of results
+	  .countBy(row => row[1]) // count how many have landed in what bucket
+	  .toPairs() // to have in tuple form
+	  .sortBy(row => row[0]) // sorting by bucket number
+	  .last() // picking out the last result ie the bucket with the highest number of balls in it
+	  .first() // taking the bucket number
+    .parseInt() // converting it from string to integer
+    .value() // terminated the chain
+    
+  console.log('Ball released at 350 will likely fall in bucket ', bucket);
 }
 
+function dist4mPoint (point) {
+  // getting absolute difference from drop point and to-be-predicted point
+return Math.abs(point - predictionPoint);
+}
